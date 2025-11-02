9c01634d8f9bd5d1f9d40236db598e9aa2f097f5
HannsW09 <hannsw09@gmail.com>
2025-11-02 09:39:34 +0000

    Co-authored-by: Ona <no-reply@ona.com>

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,41 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    element_array = np.array(list).reshape(3, 3)
 
+    mean_axis1 = [element_array.mean(axis=0)]
+    mean_axis2 = [element_array.mean(axis=1)]
+    mean_flattened = element_array.mean()
+    
+    var_axis1 = [element_array.var(axis=0)]
+    var_axis2 = [element_array.var(axis=1)]
+    var_flattened = element_array.var()
 
+    std_axis1 = [element_array.std(axis=0)]
+    std_axis2 = [element_array.std(axis=1)]
+    std_flattened = element_array.std()
 
+    max_axis1 = [element_array.max(axis=0)]
+    max_axis2 = [element_array.max(axis=1)]
+    max_flattened = element_array.max()
+
+    min_axis1 = [element_array.min(axis=0)]
+    min_axis2 = [element_array.min(axis=1)]
+    min_flattened = element_array.std()
+
+    sum_axis1 = [element_array.sum(axis=0)]
+    sum_axis2 = [element_array.sum(axis=1)]
+    sum_flattened = element_array.sum()
+
+    calculations = {
+  'mean': [mean_axis1, mean_axis2, mean_flattened],
+  'variance': [var_axis1, var_axis2, var_flattened],
+  'standard deviation': [std_axis1, std_axis2, std_flattened],
+  'max': [max_axis1, max_axis2, max_flattened],
+  'min': [min_axis1, min_axis2, min_flattened],
+  'sum': [sum_axis1, sum_axis2, sum_flattened]}
 
     return calculations
